8545cb765b620a9a0ed87c382610e1f9dff16f0c
Sayon Usman <sayonusman.ma@gmail.com>
2023-05-15 19:09:01 +0300

    Deploy related error of vercel has been fixed

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const verifyJWT = (req, res, next) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     //jwt
     app.post("/access-token", (req, res) => {
@@ -82,7 +82,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
